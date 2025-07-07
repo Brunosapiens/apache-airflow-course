0ede733bd4487070fb6f5228a1e948cf71211310
Bruno Rafael <bruno.rafael.gaio@gmail.com>
2025-07-07 17:21:00 -0300

    backfilling

--- dags/04-backfilling.py
+++ dags/04-backfilling.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 # Formas de definir agendamento em DAGs
 # 1. usando os presets (@daily, @monthly)
 with DAG(dag_id='04-backfilling',
-    start_date=datetime(2025, 7, 3),  # Data atualizada para hoje
+    start_date=datetime(2025, 7, 7),  # Data atualizada para hoje
     schedule="@daily",
     catchup=True  # Executa todas as execuções desde a start_date até hoje
     ) as dag:
@@ -14,4 +14,4 @@ with DAG(dag_id='04-backfilling',
     task2 = EmptyOperator(task_id='transform')
     task3 = EmptyOperator(task_id='load')
 
-    task1 >> task2 >> task3
+    task1 >>[ task2, task3]
